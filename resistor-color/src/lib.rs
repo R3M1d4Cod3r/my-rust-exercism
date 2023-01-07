7321d450f34483b68e972a94a4d89ca926a27d35
R3M1d4Cod3r <superchristian9@gmail.com>
2023-01-07 21:43:37 +0100

    commit

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,28 +1,22 @@
-use enum_iterator::{all};
-#[derive(Debug, PartialEq, Eq)]
+use enum_iterator::{all,Sequence};
+use int_enum::IntEnum;
+#[repr(u32)]
+#[derive(Clone, Copy,Debug, PartialEq,Sequence,IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black=0,
+    Brown=1,
+    Red=2,
+    Orange=3,
+    Yellow=4,
+    Green=5,
+    Blue=6,
+    Violet=7,
+    Grey=8,
+    White=9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    //unimplemented!("convert a color into a numerical representation");
-    let mut result=0;
-    let tmp = all::<ResistorColor>().collect::<Vec<_>>();
-    for value in tmp{
-        if value == ResistorColor{
-            return result;
-        }
-        result+=1;
-    }
+     _color.int_value()
 }
 
 pub fn value_to_color_string(value: u32) -> String {
