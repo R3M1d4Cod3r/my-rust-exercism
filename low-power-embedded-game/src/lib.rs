47cef16cdf6fc58ee41335392c35fd0a207ca42e
R3M1d4Cod3r <superchristian9@gmail.com>
2023-01-08 12:38:10 +0100

    low-power

---
+++ low-power-embedded-game/src/lib.rs
@@ -0,0 +1,19 @@
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (
+        dividend/divisor,
+        dividend%divisor,
+    )
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+
+    iter.enumerate().filter(|n| n.0%2==0).map(|n| n.1)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+       self.0.abs()+self.1.abs()
+    }
+}
